c6b089f0e969cf6097b41caf0f79b139dd3d5fb3
chandini <151708229+chandinivulli@users.noreply.github.com>
2024-08-22 11:53:37 +0530

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Hello_World
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<4;i++)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
